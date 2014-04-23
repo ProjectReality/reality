5fb27e7e8c6786c5e4df776b3f8ad38394991d9b
Yann Vaillant <yann@vaillant.im>
2014-04-23 18:04:01 +0800

    start working on Entity class

--- inc/Entity.hpp
+++ inc/Entity.hpp
@@ -1,10 +1,28 @@
 #ifndef _ENTITY_HPP
 #define _ENTITY_HPP
 
+#include <string>
+#include <Ogre.h>
+
 class Entity
-{
+{   
 public:
-    Entity() {}
+    Entity(std::string _name, std::string _mesh, Ogre::SceneManager* scene);
+
+    void setPosition(float x, float y, float z);
+    void setPosition(Ogre::Vector3 pos);
+
+    void move(float x, float y, float z);
+    void move(Ogre::Vector3 mvt);
+
+    void setRotation(float yaw, float pitch, float roll);
+    void rotate(float yaw, float pitch, float roll);
+
+private:
+    std::string         name;
+    Ogre::SceneManager* scene;
+    Ogre::SceneNode*    node;
+    Ogre::Entity*       ent;
 };
 
 #endif
--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,7 +1,50 @@
 #include "Entity.hpp"
 
 
-Entity::Entity()
+Entity::Entity(std::string _name, std::string _mesh, Ogre::SceneManager* scene)
 {
+    if ((_name.length() < 1)) {
+        std::cerr << "Error: createEntity: invalid name" << std::endl;
+        return;
+    }
+    this->name = _name;
+    this->ent  = scene->createEntity(_name, _mesh);
+    this->node = scene->getRootSceneNode()->createChildSceneNode();
+    this->node->attachObject(ent);
+}
 
+void Entity::setPosition(float x, float y, float z)
+{
+   this->node->setPosition(Ogre::Vector3(x, y, z));
+}
+
+void Entity::setPosition(Ogre::Vector3 pos)
+{
+    this->node->setPosition(pos);
+}
+
+void Entity::move(float x, float y, float z)
+{
+    this->node->translate(Ogre::Vector3(x, y, z));
+}
+
+void Entity::move(Ogre::Vector3 mvt)
+{
+      this->node->translate(mvt);
+}
+
+void Entity::setRotation(float yaw, float pitch, float roll)
+{
+    Ogre::Quaternion y(Ogre::Degree(yaw), Ogre::Vector3::UNIT_Y);
+    Ogre::Quaternion p(Ogre::Degree(pitch), Ogre::Vector3::UNIT_X);
+    Ogre::Quaternion r(Ogre::Degree(roll), Ogre::Vector3::UNIT_Z);
+    this->node->setOrientation(y*p*r);
+}
+
+void Entity::rotate(float yaw, float pitch, float roll)
+{
+    Ogre::Quaternion y(Ogre::Degree(yaw), Ogre::Vector3::UNIT_Y);
+    Ogre::Quaternion p(Ogre::Degree(pitch), Ogre::Vector3::UNIT_X);
+    Ogre::Quaternion r(Ogre::Degree(roll), Ogre::Vector3::UNIT_Z);
+   this->node->rotate(y*p*r);
 }
