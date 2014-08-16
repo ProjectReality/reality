1fbc2687a67d828a89a93e8846ed10b50f8b4bc3
Yann Vaillant <yann@vaillant.im>
2014-08-16 17:48:53 +0200

    gtest working git add test/main_test.cpp git add test/main_test.cpp git add test/main_test.cpp git add test/main_test.cpp git add test/main_test.cpp git add test/main_test.cpp git add test/main_test.cpp

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,2 +1,14 @@
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
+#include <gtest/gtest.h>
+
+#include "Core.hpp"
+
+TEST(CoreTest, ArgCorrect) {
+    Core *c = new Core(3, 0);
+
+    ASSERT_EQ(3, c->get_n_arg());
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
