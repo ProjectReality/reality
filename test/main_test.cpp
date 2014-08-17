540d087e1347fd3817c60217a99994a05cafbf6e
Yann Vaillant <yann@vaillant.im>
2014-08-17 13:11:37 +0200

    put main test in one file

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,13 +1,5 @@
 #include <gtest/gtest.h>
 
-#include "Core.hpp"
-
-TEST(CoreTest, ArgCorrect) {
-    Core *c = new Core(3, 0);
-
-    ASSERT_EQ(3, c->get_n_arg());
-}
-
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
