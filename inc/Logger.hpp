d90b82e5b4260797bd985d1c0b7ad05723a2e5d4
Crome.A <crome.crome1208@hotmail.com>
2014-08-21 13:41:44 +0200

    Logger who work a little bit

--- inc/Logger.hpp
+++ inc/Logger.hpp
@@ -2,10 +2,26 @@
 #define LOGGER_HPP_
 
 #include <string>
+
+#include <boost/log/common.hpp>
+#include <boost/log/expressions.hpp>
+
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/console.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
+
+#include <boost/log/attributes/timer.hpp>
+#include <boost/log/attributes/named_scope.hpp>
+
+#include <boost/log/sources/logger.hpp>
+
+#include <boost/log/support/date_time.hpp>
+
 #include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/file.hpp>
 #include <boost/log/sources/global_logger_storage.hpp>
 #include <boost/log/sources/severity_channel_logger.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
 
 namespace logging = boost::log;
 namespace sinks = boost::log::sinks;
@@ -25,7 +41,6 @@ public:
 	static enum severity_level
 	{
 		debug,
-		normal,
 		info,
 		warning,
 		error,
@@ -43,5 +58,27 @@ public:
 public:
 	static void log(std::string msg);
 };
+
+// The formatting logic for the severity level
+template< typename CharT, typename TraitsT >
+inline std::basic_ostream< CharT, TraitsT >& operator<< (
+	std::basic_ostream< CharT, TraitsT >& strm, Logger::severity_level lvl)
+{
+	static const char* const str[] =
+	{
+		"debug",
+		"info",
+		"warning",
+		"error",
+		"fatal"
+	};
+	if (static_cast< std::size_t >(lvl) < (sizeof(str) / sizeof(*str)))
+		strm << str[lvl];
+	else
+		strm << static_cast< int >(lvl);
+	return strm;
+}
+
+
 
 #endif /*LOGGER_HPP_*/
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -2,6 +2,29 @@
 
 void Logger::log(std::string msg)
 {
-	reality_logger_mt slg;
+	logging::add_file_log("log/Global.log",
+		keywords::format = expr::stream
+		<< " [" << expr::format_date_time< attrs::timer::value_type >("Uptime", "%O:%M:%S.%f")
+		<< "] [" << expr::format_named_scope("Scope", keywords::format = "%n (%f:%l)")
+		<< "] <" << expr::attr< severity_level >("Severity")
+		<< "> " << expr::message
+		/*
+		keywords::format = expr::format("%1% [%2%] [%3%] <%4%> %5%")
+		% expr::format_date_time< boost::posix_time::ptime >("TimeStamp", "%Y-%m-%d, %H:%M:%S.%f")
+		% expr::format_date_time< attrs::timer::value_type >("Uptime", "%O:%M:%S")
+		% expr::format_named_scope("Scope", keywords::format = "%n (%f:%l)")
+		% expr::attr< severity_level >("Severity")
+		% expr::message
+		*/
+		);
+
+	logging::add_common_attributes();
+	logging::core::get()->add_thread_attribute("Scope", attrs::named_scope());
+
+
+	reality_logger_mt slg; 
+	slg.add_attribute("Uptime", attrs::timer());
+	BOOST_LOG_SEV(slg, fatal) << msg;
+	BOOST_LOG_SEV(slg, fatal) << msg;
 	BOOST_LOG_SEV(slg, fatal) << msg;
 }
