3b90556d17daff4894d4b168328f3af1a06de44e
Crome.A <crome.crome1208@hotmail.com>
2014-10-18 10:54:30 +0200

    minor modification and adding zoli-segfault :p

--- src/Hexapat.cpp
+++ src/Hexapat.cpp
@@ -1,16 +1,18 @@
 #include "Hexapat.hpp"
+#include "Logger.hpp"
 
 Hexapat::Hexapat(int _id, int _basePatt)
 {
 	id = _id;
 	basePatt = _basePatt;
+	Logger::log_tag("DebugHexapat", "Création of Hexapat number %i", id);
 }
 
 std::map<int, aruco::Marker>	Hexapat::detect(std::map<int, aruco::Marker> markerFound)
 {
 	bool isFound = false;
 	std::map<int, aruco::Marker>::iterator tmpMarker;
-
+	Logger::log_tag("DebugHexapat", "Detection function of Hexapat number %i", id);
 	for (int i = 0; i < 6; i++)
 	{
 		if ((tmpMarker = markerFound.find(basePatt + i)) != markerFound.end())
@@ -19,11 +21,10 @@ std::map<int, aruco::Marker>	Hexapat::detect(std::map<int, aruco::Marker> marker
 			{
 				isFound = true;
 				tmpMarker->second.Rvec.at<float>(0) += 60 * i;
+				markerFound[id] = tmpMarker->second;
+				Logger::log_tag("DebugHexapat", "Hexapat number %i detected !", id);
 			}
-			else
-			{
-				markerFound.erase(tmpMarker);
-			}
+			markerFound.erase(tmpMarker);
 		}
 	}
 	return markerFound;
