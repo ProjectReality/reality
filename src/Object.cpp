5d0ae9b7ade9b4b1ad3792c9e5e3d962fe7214c0
Crome.A <crome.crome1208@hotmail.com>
2014-11-19 11:15:47 +0100

    Fonction "A peut pret"

--- src/Object.cpp
+++ src/Object.cpp
@@ -18,6 +18,7 @@ Object::Object(std::string _name, std::string _mesh, Ogre::SceneManager* scene) 
 void Object::updateData(aruco::Marker info)
 {
 	double position[3], orientation[4];
+	double distance = -0.1;
 
 	info.OgreGetPoseParameters(position, orientation);
 	this->setPosition(position[0], position[1], position[2]);
@@ -25,16 +26,44 @@ void Object::updateData(aruco::Marker info)
 	Ogre::Quaternion y(Ogre::Degree(this->getBaseYaw()), Ogre::Vector3::UNIT_Z);
 	Ogre::Quaternion p(Ogre::Degree(this->getBasePitch()), Ogre::Vector3::UNIT_Y);
 	Ogre::Quaternion r(Ogre::Degree(this->getBaseRoll()), Ogre::Vector3::UNIT_X);
+
+
+
 	if (info.ssize > 19999)
 	{
+		double mat[16];		// numérotée en colonne puis ligne 	
+		double Y_quat = orientation[2];
+		double w = orientation[0];
+		double x = orientation[1];
+		double z = orientation[3];
+		mat[0] = 1 - 2 * ((Y_quat*Y_quat) + (z*z));
+		mat[1] = 2 * ((x*Y_quat) - (z*w));
+		mat[2] = 2 * ((x*z) + (Y_quat*w));
+		mat[4] = 2 * ((x*Y_quat) + (z*w));
+		mat[5] = 1 - 2 * ((x*x) + (z*z));
+		mat[6] = 2 * ((Y_quat*z) - (x*w));
+		mat[8] = 2 * ((x*z) - (Y_quat*w));
+		mat[9] = 2 * ((Y_quat*z) + (x*w));
+		mat[10] = 1 - 2 * ((x*x) + (Y_quat*Y_quat));
+		mat[3] = mat[7] = mat[11] = 0;
+		mat[15] = 1;
+		double x1 = 0;
+		double y1 = 0;
+		double z1 = 0.2;
+		double z2 = 0.005;
+		double z3 = -0.1;
+		double newpos_x = position[0] + (x1*mat[0]) + (y1*mat[4]) + (z1*mat[8]);
+		double newpos_y = position[1] + (x1*mat[1]) + (y1*mat[5]) + (z2*mat[9]);
+		double newpos_z = position[2] + (x1*mat[2]) + (y1*mat[6]) + (z3*mat[10]);
+		this->setPosition(newpos_x, newpos_y, newpos_z);
 		Ogre::Quaternion roll(Ogre::Degree(60 * info.ssize - 20000), Ogre::Vector3::UNIT_X);
 		this->setOrientation(q * roll);
 	}
 	else
 		this->setOrientation(q);
 	this->rotate(this->getBaseYaw(), this->getBasePitch(), this->getBaseRoll());
-}
 
+}
 int Object::getPatt_id() const
 {
     return patt_id;
